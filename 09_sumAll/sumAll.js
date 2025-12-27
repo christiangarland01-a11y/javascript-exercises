df0ab77ce2f132f90866cf7fd5ad07f8dcce066e
Christian Garland <239896361+christiangarland01-a11y@users.noreply.github.com>
2025-12-27 16:55:18 -0600

    Complete Javascript exercise 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    if ((Number.isInteger(x) && x > 0) && (Number.isInteger(y) && y > 0)) {
+        let total = 0;
 
+        if (x - y < 0) {
+            for (let i = x; i <= y; i++) {
+                total += i;
+            }
+        } else {
+            for (let i = y; i <= x; i++) {
+                total += i;
+            }
+        }
+
+        return total;
+    } else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
