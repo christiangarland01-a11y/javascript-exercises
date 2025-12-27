5aaed3701dbb9723a342fada15de117d4f1afcd5
Christian Garland <239896361+christiangarland01-a11y@users.noreply.github.com>
2025-12-26 19:56:29 -0600

    Complete Javascript exercise 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let splitString = str.split("");
+    let reverseString = [];
 
+    while (splitString.length > 0) {
+        reverseString.push(splitString.pop());
+    }
+
+    return reverseString.join("");
 };
 
 // Do not edit below this line
