9509f5789c101843226b1497a671287c21ba3caf
Christian Garland <239896361+christiangarland01-a11y@users.noreply.github.com>
2025-12-27 18:01:20 -0600

    Complete Javascript exercise 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  const celsiusTemp = (temp - 32) * (5/9);
+  return parseFloat(celsiusTemp.toFixed(1)); //rounds converted temp to 1 decimal place
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  const fahrenheitTemp = temp * (9/5) + 32;
+  return parseFloat(fahrenheitTemp.toFixed(1)); //rounds converted temp to 1 decimal place
 };
 
 // Do not edit below this line
