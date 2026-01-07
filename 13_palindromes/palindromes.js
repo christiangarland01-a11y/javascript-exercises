0901697f6c87c17062b87cda00d3d70de8548395
Christian Garland <239896361+christiangarland01-a11y@users.noreply.github.com>
2026-01-06 20:37:34 -0600

    Complete Javascript exercise 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let regex = /[a-zA-Z0-9]/;
+    let oldString = str.toLowerCase().split('').filter(char => regex.test(char)).join('');
+    let newString = oldString.split('').toReversed().join('');
+    return oldString === newString;
 };
 
 // Do not edit below this line
