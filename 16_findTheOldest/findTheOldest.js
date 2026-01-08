94c3f3921039e83f45b6d069171a167eaad78290
Christian Garland <239896361+christiangarland01-a11y@users.noreply.github.com>
2026-01-08 15:24:04 -0600

    Complete Javascript exercise 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+function getAge(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    };
 
+    return death - birth
+};
+
+const findTheOldest = function(people) {
+    return people.reduce((oldest, next) => {
+        let oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        let nextAge = getAge(next.yearOfBirth, next.yearOfDeath);
+        return oldestAge > nextAge ? oldest : next;
+    })
 };
 
 // Do not edit below this line
